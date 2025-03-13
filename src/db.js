65ceda634e06a82a67bbfe9daf0c5a73ce831b7d
jhoaking <peredoroj@gmail.com>
2025-03-13 13:03:00 -0400

    agrege comentarios y mejore el nombre de un archivo

--- src/db.js
+++ src/db.js
@@ -1,7 +1,7 @@
 import { createPool } from "mysql2/promise";
 import {DB_PORT,DB_USER,DB_HOST,DB_PASSWORD,DB_DATABASE}  from './config.js'
 
-
+// conexion del archivo env
 export const pool = createPool({
     port : DB_PORT,
     user : DB_USER,
--- src/schema/validacion.js
+++ src/schema/validacion.js
@@ -6,7 +6,7 @@ export const validacionEsquema = z.object({
     password : z.string().min(6)
 })
 
-
+// validar el zod para el register y login
 export const validarServicio = (input) =>{
     return validacionEsquema.safeParse(input);
 }
